9f80edfa3a08002459bcba87a992c758cc0e8b03
Timmy Nguyen <timmypass17@gmail.com>
2023-10-22 13:28:40 -0700

    Initial Commit

---
+++ OtakuSigma/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OtakuSigma
+//
+//  Created by Timmy Nguyen on 10/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OtakuSigma/OtakuSigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtakuSigmaApp.swift
+//  OtakuSigma
+//
+//  Created by Timmy Nguyen on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct OtakuSigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
